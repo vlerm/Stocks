65b3b4cf0b4061cab2588844e225a62e2a7d073b
vlerm <mr.kooper.98@mail.ru>
2022-08-19 03:24:03 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Вадим Лавор on 19.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
